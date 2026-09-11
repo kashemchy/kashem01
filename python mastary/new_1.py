93d6e8e199252937dc8b6af01ec86018ca6ae979
kashem chowdhury <kashemchowdhury3@gmail.com>
2026-09-12 04:42:20 +0600

    new some line added

--- python mastary/new_1.py
+++ python mastary/new_1.py
@@ -19,13 +19,50 @@
 #     print("ki likho shob vul")
 
 
-a = "kashem"
-print (a.title())  # first latter big
-print(a.upper())
-print(a.lower())
+# a = "kashem"
+# print (a.title())  # first latter big
+# print(a.upper())
+# print(a.lower())
 
-txt = " I like Mango"
-x = txt.replace("Mango", "Watermelon")
-print(x)
+# txt = " I like Mango"
+# x = txt.replace("Mango", "Watermelon")
+# print(x)
+
+
+
+
+# usern_input = input("What is your name? ")
+# f_name = "kashem"
+# l_name = "Chowdhury"
+# age = 30
+# txt = "I am {f_name} {l_name}. I am {age} years old". format(l_name = l_name,f_name = f_name,age = age)
+# d = "good Morning, {}. How are you?".format(usern_input)   # this is a long cut
+# c = "good Morning, Rahimm How are you?"
+# txt2 = f"I am {f_name} {l_name}. I am {age} years old"
+# print(txt)
+# print(txt2)
+
+# conditional statemnt
+
+# rain = 1
+# if rain == 1:
+#     print("Schoole jabo na")
+# else: 
+#     print("off jethai hobe......")
+
+
+# a = [1,2,3,4]
+# b = "Hello"
+
+# print("a" in "Hellp")
+
+# for item in a:
+#     print(item)
+
+for i in range(101):
+    print(f"Nasa is Hacking: {i}%")
+print(" Nasa is Hacked. Thanks!")
+
+
 
 
